80a9aed4d11aae30b8db6ff22dac274c8953ec2d
Fan Zhang <zhangfan@cac.rutgers.edu>
2013-05-02 22:11:17 +0000

    (1) Updated README; (2) Added comments to public APIs in dataspaces.h and dimes_interface.h.

--- include/dataspaces.h
+++ include/dataspaces.h
@@ -39,40 +39,218 @@
 #ifndef __DATASPACES_H_
 #define __DATASPACES_H_
 
+/**
+ * @file dataspaces.h
+ * @brief DataSpaces APIs.
+ */
+
+/**
+ * @brief Initialize dataspaces client library.
+ * 
+ * @param[in] num_peers:	Number of peers in the client application.
+ * @param[in] appid:		Unique id for client application.
+ *
+ * @return	0 indicates success.
+ */
 int dspaces_init(int num_peers, int appid);
 
+/**
+ * @brief Finalize dataspaces client library.
+ *
+ * @return	Void.
+ */
 void dspaces_finalize (void);
 
+/**
+ * @brief Set types of memory layout for multi-dimensional matrix.
+ *
+ * Type of memory layout needs to be properly set before calling put/get APIs.
+ * 
+ * The following enum type defines the types of memory layout:
+ * enum storage_type {row_major, column_major};
+ *
+ * @param[in] fst:	Type of memory layout for the matrix.
+ *
+ * @return:	Void.
+ */
 void dspaces_set_storage_type (int fst);
 
+/**
+ * @brief Return the rank for current process.
+ *
+ * For client application with num_peers processes, the returned rank
+ * values range from 0~(num_peers-1).
+ *
+ * Note: this rank value does not necessarily equal to the MPI rank if the 
+ * client application uses MPI.
+ *
+ * @return	Rank of current process.
+ */
 int dspaces_rank(void);
 
+/**
+ * @brief Return the number of peers in current application.
+ *	
+ * @return	Number of application peers.
+ */
 int dspaces_peers(void);
 
+/**
+ * @brief Blocks until all processes in the client application have reached
+ * 	  this routine.
+ *
+ * @return	Void.
+ */
 void dspaces_barrier(void);
 
+/**
+ * @brief Acquire the read lock "lock_name".
+ *
+ * Note: this is a collective routine to be invoked by a group of application 
+ * processes. When input communicator is NULL, all applications peers need
+ * to call this routine collectively. When input communicator is a valid 
+ * MPI communicator, only the application peers that belong to the communicator
+ * group need to call this routine.
+ * 
+ * @param[in] lock_name:	Name of the lock.		
+ * @param[in] comm:		Pointer to the MPI communicator.
+ *
+ * @return	Void.		
+ */
 void dspaces_lock_on_read(const char *lock_name, void *comm);
 
+/**
+ * @brief Release the read lock.
+ *
+ * Note: this is a collective routine to be invoked by a group of application 
+ * processes. When input communicator is NULL, all applications peers need
+ * to call this routine collectively. When input communicator is a valid 
+ * MPI communicator, only the application peers that belong to the communicator
+ * group need to call this routine.
+ * 
+ * @param[in] lock_name:	Name of the lock.		
+ * @param[in] comm:		Pointer to the MPI communicator.
+ *
+ * @return	Void.		
+ */
 void dspaces_unlock_on_read(const char *lock_name, void *comm);
 
+/**
+ * @brief Acquire the write lock.
+ *
+ * Note: this is a collective routine to be invoked by a group of application 
+ * processes. When input communicator is NULL, all applications peers need
+ * to call this routine collectively. When input communicator is a valid 
+ * MPI communicator, only the application peers that belong to the communicator
+ * group need to call this routine.
+ * 
+ * @param[in] lock_name:	Name of the lock.		
+ * @param[in] comm:		Pointer to the MPI communicator.
+ *
+ * @return	Void.		
+ */
 void dspaces_lock_on_write(const char *lock_name, void *comm);
 
+/**
+ * @brief Release the write lock.
+ * 
+ * Note: this is a collective routine to be invoked by a group of application 
+ * processes. When input communicator is NULL, all applications peers need
+ * to call this routine collectively. When input communicator is a valid 
+ * MPI communicator, only the application peers that belong to the communicator
+ * group need to call this routine.
+ *
+ * @param[in] lock_name:	Name of the lock.		
+ * @param[in] comm:		Pointer to the MPI communicator.
+ *
+ * @return	Void.		
+ */
 void dspaces_unlock_on_write(const char *lock_name,void *comm);
 
-int dspaces_get (const char *var_name, 
-        unsigned int ver, int size,
-        int xl, int yl, int zl, 
-        int xu, int yu, int zu, 
-        void *data);
-
+/**
+ * @brief Query the space to insert data specified by a geometric
+ * 	  descriptor.
+ * 
+ * Memory buffer pointed by pointer "data" is a sub-region of the
+ * global n-dimensional array (n <= 3) in user application, which is described
+ * by the local bounding box {(xl, yl, zl), (xu, yu, zu)}.
+ *
+ * This routine is non-blocking, and successful return of the routine does not 
+ * guarantee the completion of data transfer from client process to dataspaces 
+ * staging server. User applications need to call dspaces_put_sync to check if
+ * the most recent dspaces_put is complete or not. 
+ * 
+ * @param[in] var_name:		Name of the variable.
+ * @param[in] ver:		Version of the variable.
+ * @param[in] size:		Size (in bytes) for each element of the global
+ *				array.
+ * @param[in] xl:		x-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] yl:		y-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] zl:		z-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] xu:		x-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] yu:		y-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] zu:		z-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] data:		Pointer to user data buffer. 
+ *
+ * @return	0 indicates success.
+ */
 int dspaces_put (const char *var_name, 
         unsigned int ver, int size,
         int xl, int yl, int zl,
         int xu, int yu, int zu, 
         void *data);
 
+/**
+ * @brief Query the space to retrieve data specified by a geometric descriptor.
+ *
+ * Memory buffer pointed by pointer "data" is a sub-region of the
+ * global n-dimensional array (n <= 3) in user application, which is described
+ * by the local bounding box {(xl, yl, zl), (xu, yu, zu)}.
+ *
+ * After successful return of the routine, received data is copied into user
+ * buffer pointed by "data".
+ *
+ * @param[in] var_name:		Name of the variable.
+ * @param[in] ver:		Version of the variable.
+ * @param[in] size:		Size (in bytes) for each element of the global
+ *				array.
+ * @param[in] xl:		x-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] yl:		y-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] zl:		z-coordinate for the lower corner of the local
+ *				bounding box.
+ * @param[in] xu:		x-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] yu:		y-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] zu:		z-coordinate for the upper corner of the local
+ *				bounding box.
+ * @param[in] data:		Pointer to user data buffer. 
+ * 
+ * @return	0 indicates success.
+ */
+int dspaces_get (const char *var_name, 
+        unsigned int ver, int size,
+        int xl, int yl, int zl, 
+        int xu, int yu, int zu, 
+        void *data);
+
+/**
+ * @brief Block till the completion of most recent data insert query.
+ *
+ * Note: it is recommended to invoke dspaces_put_sync after each call of 
+ * dspaces_put, in order to ensure correct insertion of data into dataspaces.
+ *
+ * @return	0 indicates success.
+ */
 int dspaces_put_sync(void);
 
-void dspaces_finalize (void);
-
 #endif
+
