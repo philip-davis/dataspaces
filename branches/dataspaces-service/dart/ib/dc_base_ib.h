aaef3ad912869e2391c0811d9534f63e0db87683
Hoang Bui <hbui@cac.rutgers.edu>
2014-02-18 23:13:51 +0000

    indent code

--- branches/dataspaces-service/dart/ib/dc_base_ib.h
+++ branches/dataspaces-service/dart/ib/dc_base_ib.h
@@ -3,58 +3,56 @@
 
 #include "dart_rpc_ib.h"
 
-struct dart_client
-{
-  struct rpc_server *rpc_s;
+struct dart_client {
+	struct rpc_server *rpc_s;
 
-  int peer_size;
+	int peer_size;
 
-  int local_id;
+	int local_id;
 //      struct node_id *peer_tab;
-  struct node_id *cn_peers;
+	struct node_id *cn_peers;
 
-  int num_sp, num_cp, num_cp_all;
+	int num_sp, num_cp, num_cp_all;
 
-  struct node_id *self;
-  int num_unreg;
+	struct node_id *self;
+	int num_unreg;
 
-  //struct list_head peer_list; //list of peers(servers and clients)
+	//struct list_head peer_list; //list of peers(servers and clients)
 
-  int connected;
-  /* Number of compute peers in the app job. */
-  int cp_in_job;
-  /* Rank of the master peer in the app job. */
-  int cp_min_rank;
+	int connected;
+	/* Number of compute peers in the app job. */
+	int cp_in_job;
+	/* Rank of the master peer in the app job. */
+	int cp_min_rank;
 
-  int cp_barrier_req;
+	int cp_barrier_req;
 
-  /* Registration flag; 0 - unregistred, 1 - registered. */
-  int f_reg:1;
-  int f_reg_all:1;
-  int f_bar:1;
+	/* Registration flag; 0 - unregistred, 1 - registered. */
+	int f_reg:1;
+	int f_reg_all:1;
+	int f_bar:1;
 
-  void *dart_ref;
+	void *dart_ref;
 
-  int num_posted;
+	int num_posted;
 };				// //
 
 #define dc_barrier(dc)          rpc_barrier(dc->rpc_s)
 
-static inline struct node_id *
-dc_get_peer (struct dart_client *dc, int n)
+static inline struct node_id *dc_get_peer(struct dart_client *dc, int n)
 {
 
-  return rpc_server_find (dc->rpc_s, n);
+	return rpc_server_find(dc->rpc_s, n);
 
 
-  /*
-     if(n < dc->num_sp || n>=dc->cp_min_rank + dc->num_cp)
-     return dc->peer_tab + n;
-     else if(n < dc->cp_min_rank + dc->num_cp && n >= dc->cp_min_rank)
-     return (dc->peer_tab + dc->num_sp) + (n - dc->cp_min_rank);
-     else
-     return (dc->peer_tab + dc->num_cp + n);
-   */
+	/*
+	   if(n < dc->num_sp || n>=dc->cp_min_rank + dc->num_cp)
+	   return dc->peer_tab + n;
+	   else if(n < dc->cp_min_rank + dc->num_cp && n >= dc->cp_min_rank)
+	   return (dc->peer_tab + dc->num_sp) + (n - dc->cp_min_rank);
+	   else
+	   return (dc->peer_tab + dc->num_cp + n);
+	 */
 }				// //
 
 /*
@@ -69,14 +67,13 @@ static inline struct node_id *dc_node_find(struct dart_client *dc, int nodeid)
 }
 */
 
-static inline struct dart_client *
-dc_ref_from_rpc (struct rpc_server *rpc_s)
+static inline struct dart_client *dc_ref_from_rpc(struct rpc_server *rpc_s)
 {
-  return rpc_s->dart_ref;
+	return rpc_s->dart_ref;
 }				// //
 
-struct dart_client *dc_alloc (int num_peers, int appid, void *dart_ref);	// //
-void dc_free (struct dart_client *dc);	// //
-int dc_process (struct dart_client *dc);	// //
+struct dart_client *dc_alloc(int num_peers, int appid, void *dart_ref);	// //
+void dc_free(struct dart_client *dc);	// //
+int dc_process(struct dart_client *dc);	// //
 
 #endif
--- branches/dataspaces-service/dart/ib/ds_base_ib.h
+++ branches/dataspaces-service/dart/ib/ds_base_ib.h
@@ -9,88 +9,83 @@
   Structure to represent an application that uses the Spaces.
 */
 
-struct app_info
-{
-  struct list_head app_entry;
+struct app_info {
+	struct list_head app_entry;
 
-  char *app_name;		/* Application name */
-  int app_id;			/* Application identifier */
+	char *app_name;		/* Application name */
+	int app_id;		/* Application identifier */
 
-  int app_min_id;
+	int app_min_id;
 
-  int app_num_peers;		/* Total number of peers in app */
-  struct node_id *app_peer_tab;	/* Reference to app nodes info */
+	int app_num_peers;	/* Total number of peers in app */
+	struct node_id *app_peer_tab;	/* Reference to app nodes info */
 
-  int app_cnt_peers;		/* Peers so far */
+	int app_cnt_peers;	/* Peers so far */
 };
 
-struct dart_server
-{
-  struct rpc_server *rpc_s;
+struct dart_server {
+	struct rpc_server *rpc_s;
 
-  /* List (array) of peer nodes; this should be of fixed size. */
-  int peer_size;
+	/* List (array) of peer nodes; this should be of fixed size. */
+	int peer_size;
 
-  int current_client_size;
+	int current_client_size;
 
 //      struct node_id *peer_tab;
-  struct node_id *cn_peers;
+	struct node_id *cn_peers;
 
-  int s_connected;
+	int s_connected;
 
-  /* Number of compute node peers; number of server peers. */
-  int num_cp, num_sp;
-  int size_cp, size_sp;
+	/* Number of compute node peers; number of server peers. */
+	int num_cp, num_sp;
+	int size_cp, size_sp;
 
-  struct list_head app_list;	/* List of applications */
-
-
-  //struct list_head peer_list; //list of peers(servers and clients)
+	struct list_head app_list;	/* List of applications */
 
 
-  int connected;
+	//struct list_head peer_list; //list of peers(servers and clients)
 
-  /* Reference for self instance in 'peer_tab'. */
-  struct node_id *self;
 
-  /* 'f_reg' records if registration is complete. */
-  int f_reg;
-  int f_stop;
+	int connected;
 
-  int f_unreg;
-  int num_charge;
+	/* Reference for self instance in 'peer_tab'. */
+	struct node_id *self;
 
-  int f_s_unreg;
+	/* 'f_reg' records if registration is complete. */
+	int f_reg;
+	int f_stop;
 
-  /* Flag to accept new requests or drom exsiting ones. */
-  int f_nacc:1;
+	int f_unreg;
+	int num_charge;
 
-  /* Reference to the front end module used. */
-  void *dart_ref;
+	int f_s_unreg;
+
+	/* Flag to accept new requests or drom exsiting ones. */
+	int f_nacc:1;
+
+	/* Reference to the front end module used. */
+	void *dart_ref;
 };				// //
 
-struct dart_server *ds_alloc (int num_sp, int num_cp, void *dart_ref);	// //
-void ds_free (struct dart_server *ds);	// //
-int ds_process (struct dart_server *ds);
+struct dart_server *ds_alloc(int num_sp, int num_cp, void *dart_ref);	// //
+void ds_free(struct dart_server *ds);	// //
+int ds_process(struct dart_server *ds);
 
-static inline struct dart_server *
-ds_ref_from_rpc (struct rpc_server *rpc_s)
+static inline struct dart_server *ds_ref_from_rpc(struct rpc_server *rpc_s)
 {
-  return rpc_s->dart_ref;
+	return rpc_s->dart_ref;
 }				// //
 
-static inline int
-ds_get_rank (struct dart_server *ds)
+static inline int ds_get_rank(struct dart_server *ds)
 {
-  return ds->self->ptlmap.id;
+	return ds->self->ptlmap.id;
 }				// //
 
-static inline struct node_id *
-ds_get_peer (struct dart_server *ds, int n)
+static inline struct node_id *ds_get_peer(struct dart_server *ds, int n)
 {
-  return rpc_server_find (ds->rpc_s, n);
+	return rpc_server_find(ds->rpc_s, n);
 
-  //      return (ds->peer_tab + n);
+	//      return (ds->peer_tab + n);
 }				// //
 
 /*
@@ -107,10 +102,9 @@ static inline struct node_id *ds_node_find(struct dart_server *ds, int nodeid)
 
 
 
-static inline int
-ds_stop (struct dart_server *ds)
+static inline int ds_stop(struct dart_server *ds)
 {
-  return ds->f_stop;
+	return ds->f_stop;
 }				// //
 
 #endif
