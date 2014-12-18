4dce125bdd27045b618109a0ae4cf9a404b21660
Melissa Romanus <melissa@cac.rutgers.edu>
2014-12-18 18:27:29 +0000

    test case 2d array

--- examples/ex1_putget/put.c
+++ examples/ex1_putget/put.c
@@ -7,6 +7,37 @@
 #include <stdint.h>
 #include "dataspaces.h"
 #include "mpi.h"
+#define MATRIX_DIM 10
+
+int** createMatrix(int xdim, int ydim){
+	int** mat;
+
+	if(xdim != ydim){
+		printf("ERROR: Only square matrices supported.");
+		exit(1);
+	}
+
+	mat = (int**) malloc(xdim*sizeof(int*));
+
+	int i;
+	for(i=0; i<xdim; i++){
+		mat[i] = (int*) malloc(ydim*sizeof(int));
+	}
+
+	int j, k;
+
+	
+	srand(time(NULL));
+
+	for(j=0;j<xdim;j++){
+		for(k=0;k<ydim;k++){
+			mat[j][k] = rand()%32768;
+		}
+	}
+
+	return mat; 
+}
+
 
 int main(int argc, char **argv)
 {
@@ -27,56 +58,38 @@ int main(int argc, char **argv)
 	// Pointer to the MPI Communicator, allows DS Layer to use MPI barrier func
 	// Addt'l parameters: Placeholder for future arguments, currently NULL.
 	dspaces_init(1, 1, &gcomm, NULL);
+	
+	// DataSpaces: Lock Mechanism
+	// Usage: Prevent other process from modifying 
+	// 	  data at the same time as ours
+	dspaces_lock_on_write("my_test_lock", &gcomm);
 
-	int timestep=0;
 
-	while(timestep<10){
-		timestep++;
-		sleep(2);
-		// DataSpaces: Lock Mechanism
-		// Usage: Prevent other process from modifying 
-		// 	  data at the same time as ours
-		dspaces_lock_on_write("my_test_lock", &gcomm);
+	//Name the Data that will be writen
+	char var_name[128];
+	sprintf(var_name, "matrix_A");
 
-		//Name the Data that will be writen
-		char var_name[128];
-		sprintf(var_name, "ex1_sample_data");
+	// Create our 2D matrix, 500x500
+	int **matA = createMatrix(MATRIX_DIM, MATRIX_DIM);
 
-		// Create integer array, size 3
-		int *data = malloc(3*sizeof(int));
-
-		// Initialize Random Number Generator
-		srand(time(NULL));
-
-		// Populate data array with random values from 0 to 99
-		data[0] = rand()%100;
-		data[1] = rand()%100;
-		data[2] = rand()%100;
+	// ndim: Dimensions for application data domain
+	// In this case, our matrix is 2 dimensional
+	int ndim = 2; 
+			
+	// Prepare LOWER and UPPER bound dimensions
+	uint64_t lb[3] = {0}, ub[3] = {0};
+	ub[0] = ub[1] = MATRIX_DIM-1;
+	
+	// DataSpaces: Put data array into the space
+	// Usage: dspaces_put(Name of variable, version num, 
+	// size (in bytes of each element), dimensions for bounding box,
+	// lower bound coordinates, upper bound coordinates,
+	// ptr to data buffer 
+	dspaces_put(var_name, 0, sizeof(int), ndim, lb, ub, *matA);
 		
-		printf("Timestep %d: put data %d %d %d\n", 
-			timestep, data[0], data[1], data[2]);
-		
-		// ndim: Dimensions for application data domain
-		// In this case, our data array is 1 dimensional
-		int ndim = 1; 
-		
-		// Prepare LOWER and UPPER bound dimensions
-		// In this example, we will put all data into a 
-		// small box at the origin upper bound = lower bound = (0,0,0)
-		// In further examples, we will expand this concept.
-		uint64_t lb[3] = {0}, ub[3] = {0};
-
-		// DataSpaces: Put data array into the space
-		// Usage: dspaces_put(Name of variable, version num, 
-		// size (in bytes of each element), dimensions for bounding box,
-		// lower bound coordinates, upper bound coordinates,
-		// ptr to data buffer 
-		dspaces_put(var_name, timestep, 3*sizeof(int), ndim, lb, ub, data);
-		
-		// DataSpaces: Release our lock on the data
-		dspaces_unlock_on_write("my_test_lock", &gcomm);
-	}
-
+	// DataSpaces: Release our lock on the data
+	dspaces_unlock_on_write("my_test_lock", &gcomm);
+	
 	// DataSpaces: Finalize and clean up DS process
 	dspaces_finalize();
 
