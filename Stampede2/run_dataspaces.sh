80741b36fd812547384f30767c0748126bba004a
qinyubo <qybo123@gmail.com>
2017-10-15 14:33:27 -0400

    Use environment parameter IBRUN_TASKS_PER_NODE to set PPN, and modify script, added more comments

--- Stampede2/run_dataspaces.sh
+++ Stampede2/run_dataspaces.sh
@@ -1,45 +1,54 @@
-#!/bin/bash
-#SBATCH -p TBD
-#SBATCH -A YOUR_ALLOC
-#SBATCH -N TBD
-#SBATCH -t TBD
-#SBATCH -C TBD
 
 #Stampede2
+#
+# Use ibrun MPI wrap
+# Use global environment parameter 
+# IBRUN_TASKS_PER_NODE to set processes per node
+#
+#
 
 ###########################
 # GET PARAMETER FROM CONF #
 ###########################
 
-#Array to store input data from file
+#
+# Array to store input data from file
+#
+
+# Data size array
 DATA_SIZE_X_ARR=
 DATA_SIZE_Y_ARR=
 DATA_SIZE_Z_ARR=
 
-PPN_ARR=  #Processes per node
+# Processes per node array
+PPN_ARR=  
 
 NUM_SERVER_ARR= 
 NUM_WRITER_ARR=
 NUM_READER_ARR=
 
-#Global data size
+# Global data size
 DATA_SIZE_X=
 DATA_SIZE_Y=
 DATA_SIZE_Z=
 
-NDIM=3
-PPN=1 #Processes per node
+NDIM=3 #DataSpaces dimension
+PPN=1 #Processes per node, defaul is 1
+
+#
+# DataSpaces arguments
+#
+
 NUM_SERVER= 
 NUM_CLIENT=
 NUM_WRITER=
 NUM_READER=
 
-NUM_SCALE=1
 METHOD="DATASPACES"
-ID_WRITER=1
+ID_WRITER=1 
 ID_READER=2
 
-#number of process in writer/reader each dimension
+# Number of process in writer/reader each dimension
 NUM_PROC_W_X=
 NUM_PROC_W_Y=
 NUM_PROC_W_Z=
@@ -62,16 +71,6 @@ WRITER_NODE=
 READER_NODE=
 
 
-
-
-#############################
-# DETECT SYSTEM ENVIRONMENT #
-#############################
-
-PHYSICAL_CORES_PER_NODE_HASWELL=32
-LOGICAL_CORES_PER_NODE_HASWELL=64
-PHYSICAL_CORES_PER_NODE_KNL=68
-LOGICAL_CORES_PER_NODE_KNL=272
 
 ###################################################
 # From http://phodd.net/gnu-bc/bcfaq.html#bashlog #
@@ -215,18 +214,15 @@ function write_dataspaces_conf {
 function read_input_to_array {
 	#Get data size
 	# if found keyword, then from the next line, get and process content, till match then next keyword
-DATA_SIZE_X_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[1]}' $CONFIG_FILE))
-DATA_SIZE_Y_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[2]}' $CONFIG_FILE))
-DATA_SIZE_Z_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[3]}' $CONFIG_FILE))
+	DATA_SIZE_X_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[1]}' $CONFIG_FILE))
+	DATA_SIZE_Y_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[2]}' $CONFIG_FILE))
+	DATA_SIZE_Z_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[3]}' $CONFIG_FILE))
 
-PPN_ARR=($(awk '/PPN_START:/{f=1;next}/PPN_END/{f=0}f{split($0,ft);print ft[1]}' $CONFIG_FILE))
+	PPN_ARR=($(awk '/PPN_START:/{f=1;next}/PPN_END/{f=0}f{split($0,ft);print ft[1]}' $CONFIG_FILE))
 
-NUM_SERVER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[2]}' $CONFIG_FILE))
-NUM_WRITER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[1]}' $CONFIG_FILE))
-NUM_READER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[3]}' $CONFIG_FILE))
-
-#SYSTEM=$(awk -F '=' '/^SYSTEM/{gsub(/ /, "", $2);print $2}' "${CONFIG_FILE}") #gsub to remove space
-#TIME_LIMIT=$(awk -F '=' '/^TIME_LIMIT/{gsub(/ /, "", $2);print $2}' "${CONFIG_FILE}")
+	NUM_SERVER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[2]}' $CONFIG_FILE))
+	NUM_WRITER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[1]}' $CONFIG_FILE))
+	NUM_READER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[3]}' $CONFIG_FILE))
 
 }
 
@@ -234,33 +230,32 @@ NUM_READER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":"
 # ibrun options: https://github.com/glennklockwood/ibrun/blob/master/ibrun
 function run_dataspaces {
 	NUM_CLIENT=$(($NUM_WRITER+$NUM_READER))
-	#SERVER_NODE=$(ceiling_divide $NUM_SERVER $PPN)
-	#WRITER_NODE=$(ceiling_divide $NUM_WRITER $PPN)
-	#READER_NODE=$(ceiling_divide $NUM_READER $PPN)
+
+	export IBRUN_TASKS_PER_NODE=$PPN #set the number of processes per node
 
 
 	#init
 	parse_config_writer $NUM_WRITER
 	parse_config_reader $NUM_READER
 	write_dataspaces_conf
-	#srun_config
 
-	## Server start
-	ibrun -n $NUM_SERVER -npernode $PPN .$DATASPACES_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT &>> $SCRIPT_DIR/output/log.server &
-
+	
 	## WAIT FOR SERVER START TO COMPLETE
 	sleep 1s
 	while [ ! -f conf ]; do
     	sleep 1s
 	done
-	sleep 10s  # wait server to fill up the conf file
+	sleep 5s  # wait server to fill up the conf file
+
+	## Server start
+	ibrun -n $NUM_SERVER $DATASPACES_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT &>> $SCRIPT_DIR/output/log.server &
+
 
 	## Writer start
-	ibrun -n $NUM_WRITER -npernode $PPN .$DATASPACES_DIR/test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER &>> $SCRIPT_DIR/output/log.writer &
+	ibrun -n $NUM_WRITER $DATASPACES_DIR/test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER &>> $SCRIPT_DIR/output/log.writer &
 
 	## Reader start 
-	ibrun -n $NUM_READER -npernode $PPN .$DATASPACES_DIR/test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER &>> $SCRIPT_DIR/output/log.reader &
-
+	ibrun -n $NUM_READER $DATASPACES_DIR/test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER &>> $SCRIPT_DIR/output/log.reader &
 
 	wait
 
@@ -295,19 +290,12 @@ function dummy_run {
 	write_dataspaces_conf
 	srun_config
 
-	echo "ibrun -n $NUM_SERVER -npernode $PPN ./dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT"
-
 	## Server start
-	#ibrun -n $NUM_SERVER -npernode $PPN .$DATA_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT >& $DATA_DIR/log.server &
-	echo "ibrun -n $NUM_WRITER -npernode $PPN ./test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER"
-
+	echo "ibrun -n $NUM_SERVER -npernode $PPN ./dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT"
 	## Writer start
-	#ibrun -n $NUM_WRITER -npernode $PPN .$DATA_DIR/test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER >& $DATA_DIR/log.writer &
-
+	echo "ibrun -n $NUM_WRITER -npernode $PPN ./test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER"
+	## Reader start
 	echo "ibrun -n $NUM_READER -npernode $PPN ./test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER"
-	## Reader start 
-	#ibrun -n $NUM_READER -npernode $PPN .$DATA_DIR/test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER >& $DATA_DIR/log.reader &
-
 
 }
 
