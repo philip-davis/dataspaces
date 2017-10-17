2919be1460c598dfb5d7cf6e39d313dba6871b18
qinyubo <qybo123@gmail.com>
2017-10-17 17:15:11 -0400

    Add run_with_timeout function

--- Stampede2/run_dataspaces.sh
+++ Stampede2/run_dataspaces.sh
@@ -1,54 +1,49 @@
+#!/bin/bash
+#SBATCH -J DS_test_suite
+#SBATCH -o DS_test_suite.%J.stdout
+#SBATCH -e DS_test_suite.%J.stderr
+#SBATCH -p development
+#SBATCH -N 4
+#SBATCH -n 30
+#SBATCH -t 00:10:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=qybo123@gmail.com
 
-#Stampede2
-#
-# Use ibrun MPI wrap
-# Use global environment parameter 
-# IBRUN_TASKS_PER_NODE to set processes per node
-#
-#
+
+#Caliburn
 
 ###########################
 # GET PARAMETER FROM CONF #
 ###########################
 
-#
-# Array to store input data from file
-#
-
-# Data size array
+#Array to store input data from file
 DATA_SIZE_X_ARR=
 DATA_SIZE_Y_ARR=
 DATA_SIZE_Z_ARR=
 
-# Processes per node array
-PPN_ARR=  
+PPN_ARR=  #Processes per node
 
 NUM_SERVER_ARR= 
 NUM_WRITER_ARR=
 NUM_READER_ARR=
 
-# Global data size
+#Global data size
 DATA_SIZE_X=
 DATA_SIZE_Y=
 DATA_SIZE_Z=
 
-NDIM=3 #DataSpaces dimension
-PPN=1 #Processes per node, defaul is 1
-
-#
-# DataSpaces arguments
-#
-
+NDIM=3
+#PPN=8 #Processes per node
 NUM_SERVER= 
 NUM_CLIENT=
 NUM_WRITER=
 NUM_READER=
-
+NUM_SCALE=1
 METHOD="DATASPACES"
-ID_WRITER=1 
+ID_WRITER=1
 ID_READER=2
 
-# Number of process in writer/reader each dimension
+#number of process in writer/reader each dimension
 NUM_PROC_W_X=
 NUM_PROC_W_Y=
 NUM_PROC_W_Z=
@@ -71,6 +66,17 @@ WRITER_NODE=
 READER_NODE=
 
 
+
+
+
+#############################
+# DETECT SYSTEM ENVIRONMENT #
+#############################
+
+PHYSICAL_CORES_PER_NODE_HASWELL=24
+LOGICAL_CORES_PER_NODE_HASWELL=48
+PHYSICAL_CORES_PER_NODE_KNL=68
+LOGICAL_CORES_PER_NODE_KNL=272
 
 ###################################################
 # From http://phodd.net/gnu-bc/bcfaq.html#bashlog #
@@ -201,12 +207,12 @@ function write_dataspaces_conf {
 	rm -f $SCRIPT_DIR/conf $SCRIPT_DIR/cred $SCRIPT_DIR/dataspaces.conf
 
 	echo "## Config file for DataSpaces
-	ndim = $NDIM 
-	dims = ${DATA_SIZE_X},${DATA_SIZE_Y},${DATA_SIZE_Z}
+        ndim = $NDIM
+        dims = ${DATA_SIZE_X},${DATA_SIZE_Y},${DATA_SIZE_Z}
 
-	max_versions = 1
-	lock_type = 2
-	" > $DATASPACES_DIR/dataspaces.conf
+        max_versions = 1
+        lock_type = 2
+        " > $SCRIPT_DIR/dataspaces.conf
 }
 
 
@@ -214,53 +220,48 @@ function write_dataspaces_conf {
 function read_input_to_array {
 	#Get data size
 	# if found keyword, then from the next line, get and process content, till match then next keyword
-	DATA_SIZE_X_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[1]}' $CONFIG_FILE))
-	DATA_SIZE_Y_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[2]}' $CONFIG_FILE))
-	DATA_SIZE_Z_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[3]}' $CONFIG_FILE))
+DATA_SIZE_X_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[1]}' $CONFIG_FILE))
+DATA_SIZE_Y_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[2]}' $CONFIG_FILE))
+DATA_SIZE_Z_ARR=($(awk '/DATA_SIZE_START:/{f=1;next}/DATA_SIZE_END/{f=0}f{split($0,ft,",");print ft[3]}' $CONFIG_FILE))
 
-	PPN_ARR=($(awk '/PPN_START:/{f=1;next}/PPN_END/{f=0}f{split($0,ft);print ft[1]}' $CONFIG_FILE))
+PPN_ARR=($(awk '/PPN_START:/{f=1;next}/PPN_END/{f=0}f{split($0,ft);print ft[1]}' $CONFIG_FILE))
 
-	NUM_SERVER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[2]}' $CONFIG_FILE))
-	NUM_WRITER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[1]}' $CONFIG_FILE))
-	NUM_READER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[3]}' $CONFIG_FILE))
+NUM_SERVER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[2]}' $CONFIG_FILE))
+NUM_WRITER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[1]}' $CONFIG_FILE))
+NUM_READER_ARR=($(awk '/RATIO_START:/{f=1;next}/RATIO_END/{f=0}f{split($0,ft,":");print ft[3]}' $CONFIG_FILE))
+
 
 }
 
-#Run on Stampede2 with ibrun
-# ibrun options: https://github.com/glennklockwood/ibrun/blob/master/ibrun
+#Run on Caliburn with mpirun
 function run_dataspaces {
 	NUM_CLIENT=$(($NUM_WRITER+$NUM_READER))
-
-	export IBRUN_TASKS_PER_NODE=$PPN #set the number of processes per node
 
 
 	#init
 	parse_config_writer $NUM_WRITER
 	parse_config_reader $NUM_READER
 	write_dataspaces_conf
+	#srun_config
 
-	
+	## Server start
+	mpirun -n $NUM_SERVER -npernode $PPN $DATASPACES_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT &>> $SCRIPT_DIR/output/log.server &
+
 	## WAIT FOR SERVER START TO COMPLETE
 	sleep 1s
 	while [ ! -f conf ]; do
     	sleep 1s
 	done
-	sleep 5s  # wait server to fill up the conf file
-
-	## Server start
-	ibrun -n $NUM_SERVER $DATASPACES_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT &>> $SCRIPT_DIR/output/log.server &
-
+	sleep 10s  # wait server to fill up the conf file
 
 	## Writer start
-	ibrun -n $NUM_WRITER $DATASPACES_DIR/test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER &>> $SCRIPT_DIR/output/log.writer &
+	mpirun -n $NUM_WRITER -npernode $PPN $DATASPACES_DIR/test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER &>> $SCRIPT_DIR/output/log.writer &
 
 	## Reader start 
-	ibrun -n $NUM_READER $DATASPACES_DIR/test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER &>> $SCRIPT_DIR/output/log.reader 
+	mpirun -n $NUM_READER -npernode $PPN $DATASPACES_DIR/test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER &>> $SCRIPT_DIR/output/log.reader 
 
-	#remove wait and & sign in the last test_reader line
-	#Because Stampede2 test is based on DataSpaces-as-a-service branch
-	#DataSpaces server will running in the background forever waiting for clients to connect
-	#Therefore, the script won't stop and cannot start the rest of input combination
+
+
 
 
 }
@@ -272,11 +273,8 @@ function run_dataspaces {
 function dummy_run {
 
 	NUM_CLIENT=$(($NUM_WRITER+$NUM_READER))
-	SERVER_NODE=$(ceiling_divide $NUM_SERVER $PPN)
-	WRITER_NODE=$(ceiling_divide $NUM_WRITER $PPN)
-	READER_NODE=$(ceiling_divide $NUM_READER $PPN)
 
-	
+
 	echo ""
 	echo ""
 	echo "Inputs:"
@@ -291,24 +289,48 @@ function dummy_run {
 	parse_config_writer $NUM_WRITER
 	parse_config_reader $NUM_READER
 	write_dataspaces_conf
-	srun_config
+	#srun_config
+
+	echo "mpirun -n $NUM_SERVER -npernode $PPN ./dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT"
 
 	## Server start
-	echo "ibrun -n $NUM_SERVER -npernode $PPN ./dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT"
+	#mpirun -n $NUM_SERVER -npernode $PPN .$DATA_DIR/dataspaces_server -s $NUM_SERVER -c $NUM_CLIENT >& $DATA_DIR/log.server &
+	echo "mpirun -n $NUM_WRITER -npernode $PPN ./test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER"
+
 	## Writer start
-	echo "ibrun -n $NUM_WRITER -npernode $PPN ./test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER"
-	## Reader start
-	echo "ibrun -n $NUM_READER -npernode $PPN ./test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER"
+	#mpirun -n $NUM_WRITER -npernode $PPN .$DATA_DIR//test_writer $METHOD $NUM_WRITER $NDIM $NUM_PROC_W_X $NUM_PROC_W_Y $NUM_PROC_W_Z $BLK_SIZE_W_X $BLK_SIZE_W_Y $BLK_SIZE_W_Z $NUM_TS $ID_WRITER >& $DATA_DIR/log.writer &
+
+	echo "mpirun -n $NUM_READER -npernode $PPN ./test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER"
+	## Reader start 
+	#mpirun -n $NUM_READER -npernode $PPN .$DATA_DIR//test_reader $METHOD $NUM_READER $NDIM $NUM_PROC_R_X $NUM_PROC_R_Y $NUM_PROC_R_Z $BLK_SIZE_R_X $BLK_SIZE_R_Y $BLK_SIZE_R_Z $NUM_TS $ID_READER >& $DATA_DIR/log.reader &
+
 
 }
 
-
+# Usage: run_with_timeout N cmd args...
+#    or: run_with_timeout cmd args...
+# In the second case, cmd cannot be a number and the timeout will be 10 seconds.
+# Copy from https://stackoverflow.com/questions/24412721/elegant-solution-to-implement-timeout-for-bash-commands-and-functions/24413646?noredirect=1#24413646
+function run_with_timeout { 
+    local time=10
+    if [[ $1 =~ ^[0-9]+$ ]]; then time=$1; shift; fi
+    # Run in a subshell to avoid job control messages
+    ( "$@" &
+      child=$!
+      # Avoid default notification in non-interactive shell for SIGTERM
+      trap -- "" SIGTERM
+      ( sleep $time
+        kill $child 2> /dev/null ) &
+      wait $child
+    )
+}
 
 
 function main {
 
 #let's run simulation from here
 	read_input_to_array
+
 
 	for (( i=0; i<${#DATA_SIZE_X_ARR[@]}; i++)); #Data size
 	do
@@ -334,9 +356,9 @@ function main {
 				echo "Data Size: $DATA_SIZE_X, $DATA_SIZE_Y, $DATA_SIZE_Z" &>> $SCRIPT_DIR/output/log.reader
 				echo "Writer:Server:Reader: $NUM_WRITER:$NUM_SERVER:$NUM_READER" &>> $SCRIPT_DIR/output/log.reader
 
-				run_dataspaces
 				#dummy_run
-
+				run_with_timeout $TIME_LIMIT "run_dataspaces"
+				
 			done
 		done
 	done
