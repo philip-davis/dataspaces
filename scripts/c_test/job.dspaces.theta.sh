b121c367618a7d6b581051ca414ca7b83d819a1c
Melissa <melrom@users.noreply.github.com>
2018-04-03 13:25:52 -0700

    Edit Example to generic pdomain name

--- scripts/c_test/job.dspaces.theta.sh
+++ scripts/c_test/job.dspaces.theta.sh
@@ -7,12 +7,12 @@
 ## For Theta, you must provide the GNI cookie via the env var DSPACES_GNI_COOKIE or by 
 ## compiling with the --with-gni-cookie configure flag
 
-aprun -n 2 -p MELDS ./dataspaces_server -s 2 -c 4 > server.log 2>&1 < /dev/null &
+aprun -n 2 -p YOUR_PDOMAIN_ID ./dataspaces_server -s 2 -c 4 > server.log 2>&1 < /dev/null &
 
 sleep 5
 
-aprun -n 2 -p MELDS ./test_writer DATASPACES 2 3 4 4 4 64 64 64 20 1 > writer.log 2>&1 < /dev/null &
+aprun -n 2 -p YOUR_PDOMAIN_ID ./test_writer DATASPACES 2 3 4 4 4 64 64 64 20 1 > writer.log 2>&1 < /dev/null &
 
-aprun -n 2 -p MELDS ./test_reader DATASPACES 2 3 2 4 4 64 64 64 20 2 > reader.log 2>&1 < /dev/null &
+aprun -n 2 -p YOUR_PDOMAIN_ID ./test_reader DATASPACES 2 3 2 4 4 64 64 64 20 2 > reader.log 2>&1 < /dev/null &
 
 wait
