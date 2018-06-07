cfc785c00c11d0d0f8dcbed8cd77a0797ad293a4
pradsubedi <pradeep.subedi@rutgers.edu>
2018-06-07 15:55:34 -0400

    Bug fix when data get involves both rpc and shared memory
    Conflicts:
    	src/ds_gspace.c

--- src/ds_gspace.c
+++ src/ds_gspace.c
@@ -1788,8 +1788,6 @@ static int dsgrpc_obj_get(struct rpc_server *rpc_s, struct rpc_cmd *cmd)
         od = obj_data_alloc(&oh->u.o.odsc);
         if (!od)
                 goto err_out;
-
-        ssd_copy(od, from_obj);
         od->obj_ref = from_obj;
 
         msg = msg_buf_alloc(rpc_s, peer, 0);
