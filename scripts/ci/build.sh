e254071f5929673d2d90babcf5205b708e0bb640
Philip Davis <philip.e.davis@rutgers.edu>
2020-08-12 14:17:37 +0000

    Default path for apt cmake is /usr/bin, not /usr/local/bin...

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,7 +5,7 @@ BUILD_SYS=$1
 if [ "$1" == "cmake" ] ; then
     mkdir build
     cd build
-    /usr/local/bin/cmake .. -DCMAKE_C_COMPILER=mpicc
+    /usr/bin/cmake .. -DCMAKE_C_COMPILER=mpicc
     make
 else
     ./autogen.sh
